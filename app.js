d2082570ad4ddd80c137fa3c9d9a2eddff4b5266
Maxine <maxine@power.sh>
2022-02-15 15:12:19 -0500

    displayed the monkey art

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ artApp.getArt = function() {
     url.search = new URLSearchParams({
         key: this.apiKey,
         imgonly: true,
-        ps: 50,
+        ps: 20,
         q: 'monkey'
     });
 
@@ -20,9 +20,57 @@ artApp.getArt = function() {
         return apiRes.json();
     }).then(function(jsonRes) {
         console.log(jsonRes.artObjects);
+        artApp.displayArt(jsonRes.artObjects);
     });
-    // take that data and put ot on the page
 };
+
+// create method that takes api data and displays onto page
+artApp.displayArt = function(artArray) {
+    artArray.forEach(function(artwork) {
+        // console.log(artwork);
+
+        // save data in variables
+        const artworkTitle = artwork.title;
+        const artworkImgSrc = artwork.webImage.url;
+        const altText = artwork.longTitle;
+        const artist = artwork.principalOrFirstMaker;
+
+        // console.log(artworkTitle, artworkImgSrc, altText, artist);
+
+        // create elements, add necessary classes/data, appendChild)
+
+        // create li in which this info will be added
+        const artworkLi = document.createElement('li');
+        artworkLi.classList.add('piece');
+
+        // create h2 to hold the art title
+        const heading = document.createElement('h2');
+        heading.textContent = artworkTitle;
+
+        // create img to hold artwork img
+        const artworkImg = document.createElement('img');
+        // img element node has src and alt properties. set value
+        artworkImg.alt = altText;
+        artworkImg.src = artworkImgSrc;
+
+        // create p to hold artist name
+        const artworkP = document.createElement('p');
+        artworkP.classList.add('artist');
+        artworkP.textContent = artist;
+
+
+        // apppending things into the li
+        // artworkLi.appendChild(heading);
+        // artworkLi.appendChild(artworkImg);
+        // artworkLi.appendChild(artworkP);
+        artworkLi.append(heading, artworkImg, artworkP)
+
+        // appending the li to ul
+        const artworkUl = document.getElementById('artwork');
+        artworkUl.appendChild(artworkLi);
+        console.log(artworkLi);
+    });
+}
 
 // make and init method
 artApp.init = function() {
